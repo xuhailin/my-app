5d31e510897d2ee2523bc0bbcd88b08ca1920606
徐海林 <xu.hailin@synyi.com>
2020-04-22 14:15:03 +0800

    modify material theme

--- src/app/routes/home/home.component.ts
+++ src/app/routes/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,11 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  articles: any[] = [];
+  articles: any = {};
   shapeData = [];
-  constructor() {
-   }
+  constructor(private http: HttpClient ) { }
 
   ngOnInit() {
+    this.loadArticles();
+  }
+
+  loadArticles(): void {
+    this.http.get('assets/data/topic.json')
+      .subscribe((data) => {
+        this.articles = data;
+      });
   }
 }
